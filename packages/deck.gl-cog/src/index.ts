bae5810c2f40b963f0e3fbc38c054d0f09a31b53
Kyle Barron <kyle@developmentseed.org>
2025-12-17 15:29:59 -0500

    Rename existing file to geotiff-layer.ts

--- packages/deck.gl-cog/src/index.ts
+++ packages/deck.gl-cog/src/index.ts
@@ -3,7 +3,7 @@ export {
   getGeoTIFFProjection,
   fromGeoTransform,
 } from "./geotiff-reprojection.js";
-export { COGLayer } from "./cog-layer.js";
-export type { COGLayerProps } from "./cog-layer.js";
+export { COGLayer } from "./geotiff-layer.js";
+export type { COGLayerProps } from "./geotiff-layer.js";
 export { parseCOGTileMatrixSet } from "./cog-tile-matrix-set.js";
 export { loadRgbImage } from "./geotiff.js";
