a40e1fb6d56ad19edc1051089c97bdaa782097cc
Kyle Barron <kylebarron2@gmail.com>
2025-12-29 14:28:36 -0800

    add comment

--- packages/deck.gl-raster/src/mesh-layer/mesh-layer-fragment.glsl.ts
+++ packages/deck.gl-raster/src/mesh-layer/mesh-layer-fragment.glsl.ts
@@ -1,3 +1,12 @@
+/**
+ * This is a vendored copy of the SimpleMeshLayer's fragment shader:
+ * https://github.com/visgl/deck.gl/blob/a15c8cea047993c8a861bf542835c1988f30165c/modules/mesh-layers/src/simple-mesh-layer/simple-mesh-layer-fragment.glsl.ts
+ * under the MIT license.
+ *
+ * We edited this to remove the hard-coded texture uniform because we want to
+ * support integer and signed integer textures, not only normalized unsigned
+ * textures.
+ */
 export default /* glsl */ `#version 300 es
 #define SHADER_NAME simple-mesh-layer-fs
 
@@ -22,9 +31,8 @@ void main(void) {
     normal = normals_commonspace;
   }
 
-  // Note: I think we have to initialize color here since it's interpreted as an
-  // "inout" parameter in DECKGL_FILTER_COLOR injection.
-  vec4 color = vec4(1.0);
+  // We initialize color here before passing into DECKGL_FILTER_COLOR
+  vec4 color;
   DECKGL_FILTER_COLOR(color, geometry);
 
   vec3 lightColor = lighting_getLightColor(color.rgb, cameraPosition, position_commonspace.xyz, normal);
