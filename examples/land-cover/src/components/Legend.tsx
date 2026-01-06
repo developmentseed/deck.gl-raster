644f9fb4300f9dee72c5ed8bcf22a73da2fd9a21
Kyle Barron <kyle@developmentseed.org>
2026-01-05 21:48:04 -0500

    add reference link

--- examples/land-cover/src/components/Legend.tsx
+++ examples/land-cover/src/components/Legend.tsx
@@ -207,60 +207,80 @@ export function Legend() {
       </button>
 
       {isExpanded && (
-        <div
-          style={{
-            maxHeight: "400px",
-            overflowY: "auto",
-            paddingBottom: "8px",
-          }}
-        >
-          {LEGEND_DATA.map((category) => (
-            <div key={category.heading} style={{ marginBottom: "12px" }}>
-              <div
-                style={{
-                  fontSize: "13px",
-                  fontWeight: 600,
-                  marginBottom: "6px",
-                  color: "#333",
-                }}
-              >
-                {category.heading}
-              </div>
-              {category.items.map((item) => (
+        <>
+          <div
+            style={{
+              maxHeight: "400px",
+              overflowY: "auto",
+              paddingBottom: "8px",
+            }}
+          >
+            {LEGEND_DATA.map((category) => (
+              <div key={category.heading} style={{ marginBottom: "12px" }}>
                 <div
-                  key={item.value}
                   style={{
-                    display: "flex",
-                    alignItems: "flex-start",
-                    gap: "8px",
-                    marginBottom: "8px",
-                    fontSize: "12px",
+                    fontSize: "13px",
+                    fontWeight: 600,
+                    marginBottom: "6px",
+                    color: "#333",
                   }}
                 >
+                  {category.heading}
+                </div>
+                {category.items.map((item) => (
                   <div
+                    key={item.value}
                     style={{
-                      width: "16px",
-                      height: "16px",
-                      borderRadius: "2px",
-                      flexShrink: 0,
-                      marginTop: "2px",
-                      backgroundColor: `rgb(${item.color[0]}, ${item.color[1]}, ${item.color[2]})`,
-                      border: "1px solid rgba(0,0,0,0.1)",
+                      display: "flex",
+                      alignItems: "flex-start",
+                      gap: "8px",
+                      marginBottom: "8px",
+                      fontSize: "12px",
                     }}
-                  />
-                  <div style={{ flex: 1 }}>
-                    <div style={{ fontWeight: 500, marginBottom: "2px" }}>
-                      {item.label}
-                    </div>
-                    <div style={{ color: "#666", lineHeight: "1.3" }}>
-                      {item.description}
+                  >
+                    <div
+                      style={{
+                        width: "16px",
+                        height: "16px",
+                        borderRadius: "2px",
+                        flexShrink: 0,
+                        marginTop: "2px",
+                        backgroundColor: `rgb(${item.color[0]}, ${item.color[1]}, ${item.color[2]})`,
+                        border: "1px solid rgba(0,0,0,0.1)",
+                      }}
+                    />
+                    <div style={{ flex: 1 }}>
+                      <div style={{ fontWeight: 500, marginBottom: "2px" }}>
+                        {item.label}
+                      </div>
+                      <div style={{ color: "#666", lineHeight: "1.3" }}>
+                        {item.description}
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+                ))}
+              </div>
+            ))}
+          </div>
+          <div
+            style={{
+              marginTop: "12px",
+              fontSize: "12px",
+            }}
+          >
+            <a
+              href="https://www.mrlc.gov/data/legends/national-land-cover-database-class-legend-and-description"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{
+                color: "#0066cc",
+                textDecoration: "none",
+              }}
+            >
+              Classification Reference
+            </a>
+          </div>
+        </>
       )}
     </div>
   );
