390bb81682a3c8b1b260eb772a1c050677df9248
Kyle Barron <kyle@developmentseed.org>
2026-01-15 15:25:09 -0500

    Add `getSource` prop to MosaicLayer to do initial per-mosaic-item data loading

--- packages/deck.gl-geotiff/src/mosaic-layer/mosaic-layer.ts
+++ packages/deck.gl-geotiff/src/mosaic-layer/mosaic-layer.ts
@@ -5,18 +5,37 @@ import { TileLayer } from "@deck.gl/geo-layers";
 import type { MosaicSource } from "./mosaic-tileset-2d";
 import { MosaicTileset2D } from "./mosaic-tileset-2d";
 
-export type MosaicLayerProps<MosaicT extends MosaicSource = MosaicSource> =
-  CompositeLayerProps &
-    Pick<TileLayerProps, "extent" | "minZoom" | "maxZoom"> & {
-      /** List of mosaic sources to render */
-      sources: MosaicT[];
+export type MosaicLayerProps<
+  MosaicT extends MosaicSource = MosaicSource,
+  DataT = any,
+> = CompositeLayerProps &
+  Pick<
+    TileLayerProps,
+    | "extent"
+    | "minZoom"
+    | "maxZoom"
+    | "maxCacheByteSize"
+    | "maxCacheSize"
+    | "maxRequests"
+  > & {
+    /** List of mosaic sources to render */
+    sources: MosaicT[];
 
-      /** Render a source */
-      renderSource: (
-        source: MosaicT,
-        opts: { signal?: AbortSignal },
-      ) => Layer | LayersList | null;
-    };
+    /** Fetch data for this source. */
+    getSource?: (
+      source: MosaicT,
+      opts: { signal?: AbortSignal },
+    ) => Promise<DataT>;
+
+    /** Render a source */
+    renderSource: (
+      source: MosaicT,
+      opts: {
+        data?: DataT;
+        signal?: AbortSignal;
+      },
+    ) => Layer | LayersList | null;
+  };
 
 const defaultProps: Partial<MosaicLayerProps> = {};
 
@@ -28,37 +47,64 @@ const defaultProps: Partial<MosaicLayerProps> = {};
  */
 export class MosaicLayer<
   MosaicT extends MosaicSource = MosaicSource,
-> extends CompositeLayer<MosaicLayerProps<MosaicT>> {
+  DataT = any,
+> extends CompositeLayer<MosaicLayerProps<MosaicT, DataT>> {
   static override layerName = "MosaicLayer";
   static override defaultProps = defaultProps;
 
   renderTileLayer(
     mosaicSources: MosaicT[],
-    renderSource: MosaicLayerProps<MosaicT>["renderSource"],
+    renderSource: MosaicLayerProps<MosaicT, DataT>["renderSource"],
   ): TileLayer {
+    const {
+      id,
+      minZoom,
+      maxZoom,
+      extent,
+      maxCacheByteSize,
+      maxCacheSize,
+      maxRequests,
+    } = this.props;
+
     class MosaicTileset2DFactory extends MosaicTileset2D<MosaicT> {
       constructor(opts: any) {
         super(mosaicSources, opts);
       }
     }
 
-    return new TileLayer<{ source: MosaicT; signal?: AbortSignal }>({
-      id: "mosaic-layer",
+    return new TileLayer<{
+      source: MosaicT;
+      data?: DataT;
+      signal?: AbortSignal;
+    }>({
+      id: `mosaic-layer-${id}`,
       TilesetClass: MosaicTileset2DFactory,
-      // @ts-expect-error This errors because TilesetClass is not generic.
-      // TilesetClass returns MosaicT in `index`, but the known type is only
-      // `TileIndex`, which only defines x,y,z
-      getTileData: (data) => {
-        const { index, signal } = data;
+      minZoom,
+      maxZoom,
+      extent,
+      maxCacheByteSize,
+      maxCacheSize,
+      maxRequests,
+      getTileData: async (data) => {
+        // We hard-cast this because TilesetClass is not generic.
+        // TilesetClass returns MosaicT in `index`, but the known type is only
+        // `TileIndex`, which only defines x,y,z
+        const index = data.index as MosaicT;
+        const { signal } = data;
+        const userData =
+          this.props.getSource &&
+          (await this.props.getSource(index, { signal }));
+
         return {
           source: index,
+          data: userData,
           signal,
         };
       },
       renderSubLayers: (props) => {
         const { data } = props;
-        const { source, signal } = data;
-        return renderSource(source, { signal });
+        const { source, signal, data: userData } = data;
+        return renderSource(source, { data: userData, signal });
       },
     });
   }
