adce1dcea7c5e0b12997aa5f72aa48b776adddaa
Kyle Barron <kyle@developmentseed.org>
2026-01-26 18:53:35 -0500

    feat: Call `onError` callback on GeoTIFF parsing errors

--- packages/deck.gl-geotiff/src/cog-layer.ts
+++ packages/deck.gl-geotiff/src/cog-layer.ts
@@ -212,7 +212,14 @@ export class COGLayer<
       Boolean(changeFlags.dataChanged) || props.geotiff !== oldProps.geotiff;
 
     if (needsUpdate) {
-      this._parseGeoTIFF();
+      try {
+        this._parseGeoTIFF();
+      } catch (error) {
+        if (error instanceof Error) {
+          this.props.onError?.(error);
+        }
+        throw error;
+      }
     }
   }
 
