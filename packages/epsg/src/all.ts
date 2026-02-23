000b6616a06063a7ca64ec3d0fe9c514722493c7
Kyle Barron <kyle@developmentseed.org>
2026-02-23 16:06:37 -0500

    update build

--- packages/epsg/src/all.ts
+++ packages/epsg/src/all.ts
@@ -8,17 +8,23 @@ export type EPSGEntry = {
 export default async function loadEPSG(): Promise<EPSGEntry[]> {
   const url = new URL("./epsg.csv.gz", import.meta.url);
   const response = await fetch(url);
-  const bytes = await response.arrayBuffer();
-  return parseCsv(bytes);
+  const decompressed = response.body!.pipeThrough(
+    new DecompressionStream("gzip"),
+  );
+  const text = await new Response(decompressed).text();
+  return parseCsv(text);
 }
 
-function parseCsv(bytes: ArrayBuffer): EPSGEntry[] {
-  const decoder = new TextDecoder("utf-8");
-  const text = decoder.decode(bytes);
+function parseCsv(text: string): EPSGEntry[] {
   const lines = text.split("\n");
-  const data = lines.map((line) => {
-    const [code, wkt] = line.split(SEP);
-    return { code: Number.parseInt(code, 10), wkt };
-  });
+  const data = lines
+    .filter((line) => line.length > 0)
+    .map((line) => {
+      const sep = line.indexOf(SEP);
+      return {
+        code: Number.parseInt(line.slice(0, sep), 10),
+        wkt: line.slice(sep + 1),
+      };
+    });
   return data;
 }
