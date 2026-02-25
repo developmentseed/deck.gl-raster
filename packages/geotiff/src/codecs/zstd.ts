a34ab258018a2e30223d5fe01e99993eed476df4
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-25 08:12:03 -0500

    fix: don't do the slice

--- packages/geotiff/src/codecs/zstd.ts
+++ packages/geotiff/src/codecs/zstd.ts
@@ -2,8 +2,5 @@ import { decompress } from "fzstd";
 
 export async function decode(bytes: ArrayBuffer): Promise<ArrayBuffer> {
   const result = decompress(new Uint8Array(bytes));
-  return result.buffer.slice(
-    result.byteOffset,
-    result.byteOffset + result.byteLength,
-  ) as ArrayBuffer;
+  return result.buffer as ArrayBuffer;
 }
