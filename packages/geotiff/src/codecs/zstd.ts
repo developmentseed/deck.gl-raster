653cf424b8d8421e62e650b89dd6d9b4cad618b3
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-26 10:58:38 -0500

    feat: add hook to use a different zstd decompression

--- packages/geotiff/src/codecs/zstd.ts
+++ packages/geotiff/src/codecs/zstd.ts
@@ -1,6 +1,28 @@
-import { decompress } from "fzstd";
+import { decompress as fzstdDecompress } from "fzstd";
+
+type ZstdDecompress = (bytes: Uint8Array) => Uint8Array;
+
+let decompressFn: ZstdDecompress = fzstdDecompress;
+
+/**
+ * Set a custom zstd decompression function.
+ *
+ * By default, the pure-JavaScript `fzstd` library is used. Call this function
+ * to override with a different implementation, e.g. `@hpcc-js/wasm-zstd`:
+ *
+ * ```ts
+ * import { Zstd } from "@hpcc-js/wasm-zstd";
+ * import { setZstdDecoder } from "@developmentseed/geotiff";
+ *
+ * const zstd = await Zstd.load();
+ * setZstdDecoder((bytes) => zstd.decompress(bytes));
+ * ```
+ */
+export function setZstdDecoder(decompress: ZstdDecompress): void {
+  decompressFn = decompress;
+}
 
 export async function decode(bytes: ArrayBuffer): Promise<ArrayBuffer> {
-  const result = decompress(new Uint8Array(bytes));
+  const result = decompressFn(new Uint8Array(bytes));
   return result.buffer as ArrayBuffer;
 }
--- packages/geotiff/src/index.ts
+++ packages/geotiff/src/index.ts
@@ -1,6 +1,7 @@
 export type { RasterArray } from "./array.js";
 export { parseColormap } from "./colormap.js";
 export type { ProjJson } from "./crs.js";
+export { setZstdDecoder } from "./codecs/zstd.js";
 export type { DecodedPixels, Decoder, DecoderMetadata } from "./decode.js";
 export { decode, registry } from "./decode.js";
 export { GeoTIFF } from "./geotiff.js";
