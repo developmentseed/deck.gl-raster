d6ae3e40e76ed70115d09cdc4ec9168412f81c09
Pete Gadomski <pete.gadomski@gmail.com>
2026-02-27 17:30:15 -0500

    Revert "feat: add hook to use a different zstd decompression"
    This reverts commit 653cf424b8d8421e62e650b89dd6d9b4cad618b3.

--- packages/geotiff/src/codecs/zstd.ts
+++ packages/geotiff/src/codecs/zstd.ts
@@ -1,28 +1,6 @@
-import { decompress as fzstdDecompress } from "fzstd";
-
-type ZstdDecompress = (bytes: Uint8Array) => Uint8Array;
-
-let decompressFn: ZstdDecompress = fzstdDecompress;
-
-/**
- * Set a custom zstd decompression function.
- *
- * By default, the pure-JavaScript `fzstd` library is used. Call this function
- * to override with a different implementation, e.g. `@hpcc-js/wasm-zstd`:
- *
- * ```ts
- * import { Zstd } from "@hpcc-js/wasm-zstd";
- * import { setZstdDecoder } from "@developmentseed/geotiff";
- *
- * const zstd = await Zstd.load();
- * setZstdDecoder((bytes) => zstd.decompress(bytes));
- * ```
- */
-export function setZstdDecoder(decompress: ZstdDecompress): void {
-  decompressFn = decompress;
-}
+import { decompress } from "fzstd";
 
 export async function decode(bytes: ArrayBuffer): Promise<ArrayBuffer> {
-  const result = decompressFn(new Uint8Array(bytes));
+  const result = decompress(new Uint8Array(bytes));
   return result.buffer as ArrayBuffer;
 }
