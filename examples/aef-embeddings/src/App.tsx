c49cfc8ae7212881449c3a59307c0870a7a51b47
Pete Gadomski <pete.gadomski@gmail.com>
2026-03-03 09:32:15 -0700

    fix: midpoint

--- examples/aef-embeddings/src/App.tsx
+++ examples/aef-embeddings/src/App.tsx
@@ -162,8 +162,8 @@ export default function App() {
     <div style={{ width: "100%", height: "100%", position: "relative" }}>
       <MaplibreMap
         initialViewState={{
-          longitude: -105.1,
-          latitude: 40.17,
+          longitude: -105.48481514871036,
+          latitude: 40.33030130607851,
           zoom: 10,
         }}
         style={{ width: "100%", height: "100%" }}
