102af345895d39cdd1083185589d137115d1da0b
Kyle Barron <kyle@developmentseed.org>
2026-03-05 16:32:43 -0500

    make copy into JS memory after lzw decoder

--- packages/geotiff/src/codecs/lzw.ts
+++ packages/geotiff/src/codecs/lzw.ts
@@ -8,5 +8,13 @@ export async function decode(
   const { width, height, samplesPerPixel, bitsPerSample } = metadata;
   const maxUncompressedSize =
     width * height * samplesPerPixel * (bitsPerSample / 8);
-  return decompress(bytes, maxUncompressedSize);
+  const decompressed = decompress(bytes, maxUncompressedSize);
+
+  // decompressed is a view over WebAssembly memory. In order to avoid problems
+  // with memory management (especially around transferring the backing buffer,
+  // which is actually the Wasm memory space), we copy the data into a fresh
+  // Uint8Array
+  const copy = new Uint8Array(decompressed.byteLength);
+  copy.set(decompressed);
+  return copy;
 }
