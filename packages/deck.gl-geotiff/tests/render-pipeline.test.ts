39f14ef8746bca3dc36d6e299ea522207bf5be4e
Kyle Barron <kyle@developmentseed.org>
2026-03-05 15:20:51 -0500

    Add test for inferring render pipeline with mask

--- packages/deck.gl-geotiff/tests/render-pipeline.test.ts
+++ packages/deck.gl-geotiff/tests/render-pipeline.test.ts
@@ -1,4 +1,6 @@
 import assert from "node:assert";
+import type { RasterModule } from "@developmentseed/deck.gl-raster";
+import type { GeoTIFF } from "@developmentseed/geotiff";
 import { describe, expect, it } from "vitest";
 import { loadGeoTIFF } from "../../geotiff/tests/helpers.js";
 import { inferRenderPipeline } from "../src/geotiff/render-pipeline";
@@ -10,19 +12,23 @@ const MOCK_RENDER_TILE_DATA = {
   texture: {},
 };
 
+function _createRenderPipeline(geotiff: GeoTIFF): RasterModule[] {
+  const { getTileData: _, renderTile } = inferRenderPipeline(
+    geotiff,
+    MOCK_DEVICE as any,
+  );
+  const renderPipeline = renderTile(MOCK_RENDER_TILE_DATA as any);
+
+  assert(!(renderPipeline instanceof ImageData));
+  return renderPipeline;
+}
+
 describe("land cover, single-band uint8", async () => {
   const geotiff = await loadGeoTIFF("nlcd_landcover", "nlcd");
 
   it("generates correct render pipeline", () => {
-    const { getTileData: _, renderTile } = inferRenderPipeline(
-      geotiff,
-      MOCK_DEVICE as any,
-    );
-    const renderPipeline = renderTile(MOCK_RENDER_TILE_DATA as any);
+    const renderPipeline = _createRenderPipeline(geotiff);
 
-    assert(!(renderPipeline instanceof ImageData));
-
-    expect(Array.isArray(renderPipeline)).toBeTruthy();
     expect(renderPipeline[0]?.module.name).toEqual("create-texture-unorm");
 
     expect(renderPipeline[1]?.module.name).toEqual("nodata");
@@ -30,5 +36,19 @@ describe("land cover, single-band uint8", async () => {
 
     expect(renderPipeline[2]?.module.name).toEqual("colormap");
     expect(renderPipeline[2]?.props?.colormapTexture).toBeDefined();
+  });
+});
+
+describe("RGB with mask", async () => {
+  const geotiff = await loadGeoTIFF(
+    "maxar_opendata_yellowstone_visual",
+    "vantor",
+  );
+
+  it("generates correct render pipeline", () => {
+    const renderPipeline = _createRenderPipeline(geotiff);
+
+    expect(renderPipeline[0]?.module.name).toEqual("create-texture-unorm");
+    expect(renderPipeline[1]?.module.name).toEqual("mask-texture");
   });
 });
