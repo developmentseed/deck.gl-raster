9517b4694d44a4e27f6b8fda5ab71ad426e8bc6e
Kyle Barron <kyle@developmentseed.org>
2026-03-05 16:45:37 -0500

    revert predictor

--- packages/geotiff/src/codecs/predictor.ts
+++ packages/geotiff/src/codecs/predictor.ts
@@ -62,14 +62,14 @@ function decodeRowFloatingPoint(
  * @param planarConfiguration  PlanarConfiguration enum value.
  */
 export function applyPredictor(
-  block: Uint8Array,
+  block: ArrayBuffer,
   predictor: Predictor,
   width: number,
   height: number,
   bitsPerSample: number,
   samplesPerPixel: number,
   planarConfiguration: PlanarConfiguration,
-): Uint8Array {
+): ArrayBuffer {
   if (predictor === Predictor.None) {
     return block;
   }
@@ -79,8 +79,8 @@ export function applyPredictor(
     planarConfiguration === PlanarConfiguration.Separate ? 1 : samplesPerPixel;
 
   for (let i = 0; i < height; i++) {
-    const byteOffset = block.byteOffset + i * stride * width * bytesPerSample;
-    if (byteOffset >= block.byteOffset + block.byteLength) {
+    const byteOffset = i * stride * width * bytesPerSample;
+    if (byteOffset >= block.byteLength) {
       break;
     }
 
@@ -89,13 +89,13 @@ export function applyPredictor(
       const length = stride * width;
       switch (bitsPerSample) {
         case 8:
-          row = new Uint8Array(block.buffer, byteOffset, length);
+          row = new Uint8Array(block, byteOffset, length);
           break;
         case 16:
-          row = new Uint16Array(block.buffer, byteOffset, length);
+          row = new Uint16Array(block, byteOffset, length);
           break;
         case 32:
-          row = new Uint32Array(block.buffer, byteOffset, length);
+          row = new Uint32Array(block, byteOffset, length);
           break;
         default:
           throw new Error(
@@ -105,7 +105,7 @@ export function applyPredictor(
       decodeRowAcc(row, stride);
     } else if (predictor === Predictor.FloatingPoint) {
       const row = new Uint8Array(
-        block.buffer,
+        block,
         byteOffset,
         stride * width * bytesPerSample,
       );
