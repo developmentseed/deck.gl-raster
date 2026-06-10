0870e51539fbdbbb42ea4481e457a0bb850fdf2e
Kyle Barron <kyle@developmentseed.org>
2026-06-10 16:53:04 -0400

    refactor!: Flatten `texture` exports from `geotiff/texture.js`

--- packages/deck.gl-geotiff/src/index.ts
+++ packages/deck.gl-geotiff/src/index.ts
@@ -5,7 +5,7 @@ export type {
 export { COGLayer } from "./cog-layer.js";
 export { DEFAULT_CONCURRENCY_LIMITER } from "./default-concurrency-limiter.js";
 export { addAlphaChannel } from "./geotiff/geotiff.js";
-export * as texture from "./geotiff/texture.js";
+export { createTextureProps, inferTextureFormat } from "./geotiff/texture.js";
 export type { MosaicLayerProps } from "./mosaic-layer/mosaic-layer.js";
 export { MosaicLayer } from "./mosaic-layer/mosaic-layer.js";
 export type { MosaicSource } from "./mosaic-layer/mosaic-tileset-2d.js";
